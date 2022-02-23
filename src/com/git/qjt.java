7db5ee74d99d59180056a1928499a46c4517a72f
chengxuyuanxiaoqin <69973350+chengxuyuanxiaoqin@users.noreply.github.com>
2022-02-23 16:19:35 +0800

    Update qjt.java

--- src/com/git/qjt.java
+++ src/com/git/qjt.java
@@ -4,6 +4,7 @@ public class qjt {
     public static void main(String[] args) {
         System.out.println("测试！！！");
         System.out.println("测试2!!!");
-        System.out.println("热修复");
+        System.out.println("热修复111");
+        
     }
 }
